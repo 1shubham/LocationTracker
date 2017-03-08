b9229ccb131c04318094e9eea682de9ee5667566
Shubham Kaushal <shubham@chaldal.com>
2017-03-07 17:22:39 -0800

    Xamarin project setup

---
+++ LocationTracker.Core/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LocationTracker.Core
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new ContentPage() {Content = new Label {Text = "Testing..."}};
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+        }
+    }
+}
---
+++ LocationTracker.Droid/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.OS;
+using LocationTracker.Core;
+using Xamarin.Forms.Platform.Android;
+
+namespace LocationTracker.Droid
+{
+    [Activity(Label = "Location Tracker", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            
+            Xamarin.Forms.Forms.Init(this, bundle);
+            
+            LoadApplication(new App());
+        }
+    }
+}
+
